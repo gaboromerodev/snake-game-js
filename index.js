3164dcddeccbb99661fbb0e22b97ec0a29b7d234
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-12-08 13:08:39 -0300

    Picking up elements using .querySelector()

---
+++ index.js
@@ -0,0 +1,13 @@
+const grid = document.querySelector('.grid')
+const startButton = document.getElementById('start')
+const score = document.getElementById('score')
+
+function createGrid() {
+    // create elements
+
+    // create 100 of these elements
+
+    // add styling to these elements
+
+    // create array of squares
+}
