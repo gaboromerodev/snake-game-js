f9d2bd753541c8442f97c94e86cba1ff12d15ece
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-12-08 13:19:51 -0300

    Adding squares into our grid using JavaScript

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const score = document.getElementById('score')
+let squares = [
+
+]
 
 function createGrid() {
-    // create elements
+    
 
-    // create 100 of these elements
-
+    // create 100 of these elements with a for loop
+    for(let i = 0; i < 100; i++) {     
+    // create element
+    const square = document.createElement('div')
+    console.log(square)
     // add styling to these elements
-
-    // create array of squares
+    square.classList.add('square')
+    // put the element into our grid
+    grid.appendChild(square)
+    // push it into a new squares array
+    squares.push(square)
+    }
+    console.log(squares)
 }
+
+createGrid()
